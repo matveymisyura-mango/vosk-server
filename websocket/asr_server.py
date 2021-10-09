49621876e0fbee85619000ad83fc5124f23c63df
taraskinua <33247633+taraskinua@users.noreply.github.com>
2021-10-09 21:29:23 +0300

    Update asr_server.py

--- websocket/asr_server.py
+++ websocket/asr_server.py
@@ -23,6 +23,22 @@ def process_chunk(rec, message):
     else:
         return rec.PartialResult(), False
 
+def process_chunk(rec, message, frames):
+    if message == '{"eof" : 1}':
+        return rec.FinalResult(), [], False, True
+    newaudiodata = audioop.tomono(message, 2, 1, 0)
+    # right channel
+    #newaudiodata = audioop.tomono(message, 2, 0, 1)
+    if rec.AcceptWaveform(newaudiodata):
+        frames.append(newaudiodata)
+        return rec.Result(), frames, True, False
+    else:
+        frames.append(newaudiodata)
+        #return rec.PartialResult(), frames, False, False
+        return '{"partial":""}', frames, False, False
+		
+		
+		
 async def recognize(websocket, path):
     global model
     global spk_model
@@ -31,13 +47,14 @@ async def recognize(websocket, path):
     global pool
 
     rec = None
+    recfull = None
     phrase_list = None
     sample_rate = args.sample_rate
     show_words = args.show_words
     max_alternatives = args.max_alternatives
-
+    
     logging.info('Connection from %s', websocket.remote_address);
-
+    frames=[]
     while True:
 
         message = await websocket.recv()
@@ -66,9 +83,30 @@ async def recognize(websocket, path):
             rec.SetMaxAlternatives(max_alternatives)
             if spk_model:
                 rec.SetSpkModel(spk_model)
+        # Create the recognizer, word list is temporary disabled since not every model supports it
+        if not recfull:
+            if phrase_list:
+                recfull = KaldiRecognizer(model, sample_rate, json.dumps(phrase_list, ensure_ascii=False))
+            else:
+                recfull = KaldiRecognizer(model, sample_rate)
+            recfull.SetWords(show_words)
+            recfull.SetMaxAlternatives(max_alternatives)
+            if spk_model:
+                recfull.SetSpkModel(spk_model)
 
-        response, stop = await loop.run_in_executor(pool, process_chunk, rec, message)
-        await websocket.send(response)
+        #print ('start' , datetime.now())
+        response, frames, endsetense, stop = await loop.run_in_executor(pool, process_chunk, rec, message, frames)
+        #await websocket.send(response)
+        if endsetense:
+            audiodata = b''.join(frames)
+            if recfull.AcceptWaveform(audiodata):
+                await websocket.send(recfull.Result())
+            else:
+                await websocket.send(recfull.PartialResult())
+            frames.clear()
+            #print ('finish' , datetime.now())
+        else:
+            await websocket.send(response)
         if stop: break
 
 

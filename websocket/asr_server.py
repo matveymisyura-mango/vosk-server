27f343678874e639263be6412f5269a498a6af01
matveymisyura-mango <57391724+matveymisyura-mango@users.noreply.github.com>
2021-10-12 00:30:30 +0300

    Update asr_server.py

--- websocket/asr_server.py
+++ websocket/asr_server.py
@@ -151,7 +151,7 @@ def start():
     args = type('', (), {})()
 
     args.interface = os.environ.get('VOSK_SERVER_INTERFACE', '0.0.0.0')
-    args.port = int(os.environ.get('VOSK_SERVER_PORT', 2701))
+    args.port = int(os.environ.get('VOSK_SERVER_PORT', 2700))
     args.model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
     args.spk_model_path = os.environ.get('VOSK_SPK_MODEL_PATH')
     args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 8000))

a32ca7b0a97df9b3e1b1928870b592d08f067df6
matveymisyura-mango <57391724+matveymisyura-mango@users.noreply.github.com>
2021-10-12 23:07:45 +0300

    Update asr_server.py

--- websocket/asr_server.py
+++ websocket/asr_server.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-
 import json
 import os
 import sys
 import asyncio
 import pathlib
-import audioop
 import websockets
 import concurrent.futures
 import logging
-from vosk import Model, SpkModel, KaldiRecognizer
-import silence_detector
+from vosk import Model, KaldiRecognizer
 from datetime import datetime
+
 
 def process_chunk(rec, message):
     if message == '{"eof" : 1}':
@@ -19,17 +16,14 @@ def process_chunk(rec, message):
     logging.info('Start recognition frase ' + str(datetime.now()));
     leftchaneldata = audioop.tomono(message, 2, 1, 0)
     if rec.AcceptWaveform(leftchaneldata):
-        out = rec.Result()
-        rec.Reset()
         logging.info('End recognition frase ' + str(datetime.now())); 
-        return out, False
+        return rec.Result(), False
     else:
         logging.info('End recognition frase ' + str(datetime.now())); 
         return rec.PartialResult(), False
 
 async def recognize(websocket, path):
     global model
-    global spk_model
     global args
     global loop
     global pool
@@ -41,19 +35,10 @@ async def recognize(websocket, path):
     max_alternatives = args.max_alternatives
 
     logging.info('Connection from %s', websocket.remote_address);
-    
-    detector = silence_detector.SilenceDetector(silence_detector.FQ8000HZ, silence_detector.SIGNED16BIT,
-                                                silence_detector.LITTLE_ENDIAN,
-                                                silence_length=1800, volume_level=15000000, silence_to_volume_level=0.25)
 
     while True:
 
-        try:
-            message = await websocket.recv()
-        except websockets.ConnectionClosed:
-            logging.info('Socket abnormaly closed ' + str(datetime.now()));
-            break
-
+        message = await websocket.recv()
 
         # Load configuration if provided
         if isinstance(message, str) and 'config' in message:
@@ -72,34 +57,21 @@ async def recognize(websocket, path):
         # Create the recognizer, word list is temporary disabled since not every model supports it
         if not rec:
             if phrase_list:
-                rec = KaldiRecognizer(model, sample_rate, json.dumps(phrase_list, ensure_ascii=False))
+                 rec = KaldiRecognizer(model, sample_rate, json.dumps(phrase_list, ensure_ascii=False))
             else:
-                rec = KaldiRecognizer(model, sample_rate)
+                 rec = KaldiRecognizer(model, sample_rate)
             rec.SetWords(show_words)
             rec.SetMaxAlternatives(max_alternatives)
-            #if spk_model:
-            #   rec.SetSpkModel(spk_model)
 
-        if len(message) >10 :
-            if detector.is_new_silence(message):
-                await websocket.send('{"partial":"silence_detected"}')
-                continue
+        response, stop = await loop.run_in_executor(pool, process_chunk, rec, message)
+        await websocket.send(response)
+        if stop: break
 
-        try:
-            response, stop = await loop.run_in_executor(pool, process_chunk, rec, message)
-            await websocket.send(response)
-        except:
-            logging.info('When recognition socket abnormaly closed ' + str(datetime.now()));
-            break
- 
-        if stop: 
-            break
 
 
 def start():
 
     global model
-    global spk_model
     global args
     global loop
     global pool
@@ -116,14 +88,12 @@ def start():
     args.interface = os.environ.get('VOSK_SERVER_INTERFACE', '0.0.0.0')
     args.port = int(os.environ.get('VOSK_SERVER_PORT', 2700))
     args.model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
-    args.spk_model_path = os.environ.get('VOSK_SPK_MODEL_PATH')
     args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 8000))
     args.max_alternatives = int(os.environ.get('VOSK_ALTERNATIVES', 0))
     args.show_words = bool(os.environ.get('VOSK_SHOW_WORDS', True))
 
     if len(sys.argv) > 1:
        args.model_path = sys.argv[1]
-    #args.model_path = 'model-he'
 
     # Gpu part, uncomment if vosk-api has gpu support
     #
@@ -134,8 +104,6 @@ def start():
     # pool = concurrent.futures.ThreadPoolExecutor(initializer=thread_init)
 
     model = Model(args.model_path)
-    spk_model = SpkModel(args.spk_model_path) if args.spk_model_path else None
-
     pool = concurrent.futures.ThreadPoolExecutor((os.cpu_count() or 1))
     loop = asyncio.get_event_loop()
 
